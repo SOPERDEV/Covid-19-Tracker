48cfdbebf4a65cca2d8ccbce0b27782d06b5b391
Software Personification and Development <96232688+SOPERDEV-Official@users.noreply.github.com>
2022-10-03 21:17:34 +0530

    Update Guidelines.js

--- screens/Guidelines.js
+++ screens/Guidelines.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import {View,Text} from 'react-native';
+
 import {WebView} from 'react-native-webview';
 
 export default class Guidelines extends Component{
